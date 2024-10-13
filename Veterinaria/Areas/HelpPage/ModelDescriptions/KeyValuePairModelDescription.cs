57497a962d406973e6e8ccbb6168e1aee22bbae0
gatc2002 <tovargusta3@gmail.com>
2024-10-13 10:36:01 -0500

    Agregar archivos de proyecto.

---
+++ Veterinaria/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Veterinaria.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Veterinaria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Veterinaria.WebApiApplication" Language="C#" %>
