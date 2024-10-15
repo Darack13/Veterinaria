eecf50968b2eae7c6cf77e38cb69fc5a061dd2cf
Mario Gutiérrez Osorio <dainoal30@hotmail.com>
2024-10-14 19:31:39 -0500

    Creación de clase pacientes

---
+++ Veterinaria/Clases/clsPacientes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Veterinaria.Models;
+
+
+namespace Veterinaria.Clases
+{
+    public class clsPacientes
+    {
+
+        ClinicaVeterinariaEntities1 DBConection = new ClinicaVeterinariaEntities1();
+        public Paciente paciente { get; set; }
+
+        // Consultar un paciente por ID
+        public Paciente GetPacienteByID(int IdPaciente)
+        {
+            return DBConection.Pacientes.FirstOrDefault(x => x.id_paciente == IdPaciente); 
+        }
+
+        
+        // Consultar todos los pacientes
+        public List<Paciente> GetPacientes()
+        {
+            return DBConection.Pacientes
+                .OrderBy(x => x.nombre)
+                .ToList();
+        }
+
+
+        // Guardar nuevo paciente
+        public string InsertPaciente()
+        {
+            try
+            {
+                DBConection.Pacientes.Add(paciente);
+                DBConection.SaveChanges();
+
+                return "Se guardó correctamente la información del nuevo paciente";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+
+        // Eliminar paciene
+        public string DeletePaciente()
+        {
+            try
+            {
+                Paciente _paciente= GetPacienteByID(paciente.id_paciente);
+                if (_paciente == null)
+                {
+                    return "El paciente no existe en la base de datos";
+                }
+                else
+                {
+                    DBConection.Pacientes.Remove(_paciente);
+                    DBConection.SaveChanges();
+                    return "Se eliminó el registro de manera exitosa";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+
+        // Actualizar paciente
+        public string UpdatePaciente()
+        {
+            try
+            {
+                Paciente _paciente = GetPacienteByID(paciente.id_paciente);
+
+                DeletePaciente();
+                InsertPaciente();
+
+                DBConection.SaveChanges();
+                return "Se actualizaron los datos de manera correcta";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ Veterinaria/Controllers/PacienteController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veterinaria.Clases;
+using Veterinaria.Models;
+
+
+
+namespace Veterinaria.Controllers
+{
+    [RoutePrefix("api/Paciente")]
+    public class PacienteController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarPaciente")]
+        public Paciente ConsultarPaciente(int IdPaciente)
+        {
+            clsPacientes _paciente = new clsPaciente();
+            return _paciente.GetPacienteByID(IdPaciente);
+        }
+
+
+        [HttpGet]
+        [Route("ConsultarRazas")]
+        public List<Raza> ConsultarRazas()
+        {
+            clsRaza _raza = new clsRaza();
+            return _raza.GetRazas();
+        }
+
+
+        [HttpGet]
+        [Route("ConsultarRazasPorEspecie")]
+        public List<Raza> ConsultarRazasPorEspecie(int IdEspecie)
+        {
+            clsRaza _raza = new clsRaza();
+            return _raza.GetRazasBySpecy(IdEspecie);
+        }
+
+
+        [HttpPost]
+        [Route("AgregarRaza")]
+        public string InsertarRaza([FromBody] Raza DataRaza)
+        {
+            clsRaza _raza = new clsRaza();
+            _raza.raza = DataRaza; 
+
+            return _raza.InsertRaza();
+        }
+
+
+        [HttpDelete]
+        [Route("EliminarRaza")]
+        public string EliminarRaza([FromBody] Raza DataRaza)
+        {
+            clsRaza _raza = new clsRaza();
+            _raza.raza = DataRaza;
+
+            return _raza.DeleteRaza(); 
+        }
+
+
+        [HttpPut]
+        [Route("ActualizarRaza")]
+        public string ActualizarRaza([FromBody] Raza DataRaza)
+        {
+            clsRaza _raza = new clsRaza();
+            _raza.raza = DataRaza;
+
+            return _raza.UpdateRaza();
+        }
+
+    }
+}
